a615bd884d189616a56b1e04bb6167a75285c26e
zeybusra <93403757+zeybusra@users.noreply.github.com>
2021-12-28 03:49:12 +0300

    profile page worked

--- js/app.js
+++ js/app.js
@@ -2,17 +2,29 @@ const value = document.querySelector("#themeChanger");
 
 value.addEventListener("click", function (e) {
     // const changer_button = document.querySelector("#themeChanger");
-    if (value.classList.contains("light-theme")) {
-        value.classList.remove("light-theme");
-        value.classList.add("dark-theme");
+    if (document.body.classList.contains("dash-light-theme")) {
+        // value.classList.remove("light-theme");
+        // value.classList.add("dark-theme");
+        value.classList.add("dark-a");
         document.body.classList.remove("dash-light-theme");
         value.innerHTML = "<i class=\"bi bi-brightness-high fs-5 mx-3 p-2 rounded-3 \"></i>";
     } else {
-        value.classList.remove("dark-theme");
-        value.classList.add("light-theme");
+        // value.classList.remove("dark-theme");
+        // value.classList.add("light-theme");
+        // value.classList.add("light-a");
         document.body.classList.add("dash-light-theme");
         value.innerHTML = "<i class=\"bi bi-moon fs-5 mx-3 p-2 rounded-3 \"></i>";
     }
 
 }, false);
+
+
+const dashProfile = document.getElementById("profilePicture");
+
+dashProfile.addEventListener("click", pickProfile);
+
+function pickProfile() {
+
+}
+
 
