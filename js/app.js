b0423b01aeefe0bca15927b7bb52c820c92cfaab
zeybusra <93403757+zeybusra@users.noreply.github.com>
2022-01-03 23:53:31 +0300

    dark-light theme completed, profile pictured saved local storage, pp selection modal is used.

--- js/app.js
+++ js/app.js
@@ -1,17 +1,19 @@
+// VALUE
 const value = document.querySelector("#themeChanger");
+const fullScreenArea = document.documentElement;
+const fullScreenButton = document.getElementById("fullScreen");
 
+// EVENTS
+fullScreenButton.addEventListener("click", fullScreenMode);
+fullScreenButton.addEventListener("click", closeFullscreen);
+
+// FUNCTIONS
 value.addEventListener("click", function (e) {
-    // const changer_button = document.querySelector("#themeChanger");
+
     if (document.body.classList.contains("dash-light-theme")) {
-        // value.classList.remove("light-theme");
-        // value.classList.add("dark-theme");
-        value.classList.add("dark-a");
         document.body.classList.remove("dash-light-theme");
         value.innerHTML = "<i class=\"bi bi-brightness-high fs-5 mx-3 p-2 rounded-3 \"></i>";
     } else {
-        // value.classList.remove("dark-theme");
-        // value.classList.add("light-theme");
-        // value.classList.add("light-a");
         document.body.classList.add("dash-light-theme");
         value.innerHTML = "<i class=\"bi bi-moon fs-5 mx-3 p-2 rounded-3 \"></i>";
     }
@@ -19,12 +21,23 @@ value.addEventListener("click", function (e) {
 }, false);
 
 
-const dashProfile = document.getElementById("profilePicture");
-
-dashProfile.addEventListener("click", pickProfile);
-
-function pickProfile() {
-
+function fullScreenMode() {
+    if (fullScreenArea.requestFullscreen) {
+        fullScreenArea.requestFullscreen();
+    } else if (fullScreenArea.webkitRequestFullscreen) {
+        fullScreenArea.webkitRequestFullscreen();
+    } else if (fullScreenArea.msRequestFullscreen) {
+        fullScreenArea.msRequestFullscreen();
+    }
 }
 
+function closeFullscreen() {
+    if (document.exitFullscreen) {
+        document.exitFullscreen();
+    } else if (document.webkitExitFullscreen) {
+        document.webkitExitFullscreen();
+    } else if (document.msExitFullscreen) {
+        document.msExitFullscreen();
+    }
+}
 
